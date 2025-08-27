0bbd00a2775c9d4a39095d9bdfffa136f96fc08e
Carlos <carloscr95r@gmail.com>
2025-08-27 01:03:53 -0300

    update

--- src/public/scripts.js
+++ src/public/scripts.js
@@ -62,3 +62,4 @@ socket.on("products", (products) => {
     .join(" ");
   productContainer.innerHTML = productRender;
 });
+
--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -1,6 +1,23 @@
 const router = require("express").Router();
+const ProductsController = require("../controllers/products.controller");
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    res.render('home', { products: await ProductsController.getProducts() })
+});
+
+router.get('/home', async (req, res) => {
+    res.render('home', { products: await ProductsController.getProducts() })
+});
+
+router.get('/productos', (req, res) => {
+    res.render('products')
+});
+
+router.get('/products', (req, res) => {
+    res.render('products')
+});
+
+router.get('/realtimeproducts', (req, res) => {
     res.render('products')
 });
 
